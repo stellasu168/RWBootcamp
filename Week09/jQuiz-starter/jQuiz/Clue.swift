f85bf2b90e5dd8d465770d14c084460f5037cb35
Stella Su <millionstars@gmail.com>
2020-07-25 14:56:09 -0700

    Added Clue and Category structs

--- Week09/jQuiz-starter/jQuiz/Clue.swift
+++ Week09/jQuiz-starter/jQuiz/Clue.swift
@@ -9,9 +9,23 @@
 import Foundation
 
 struct Clue {
-
+    var id: Int
+    var answer : String
+    var question : String
+    var value : Int
+    var airdate: String
+    var created_at: String
+    var updated_at: String
+    var category_id: Int
+    var game_id : Int
+    var invalid_count: Int
+    var category : Category
 }
 
 struct Category {
-
+    var id : Int
+    var title : String
+    var created_at : String
+    var updated_at : String
+    var clues_count : Int
 }
